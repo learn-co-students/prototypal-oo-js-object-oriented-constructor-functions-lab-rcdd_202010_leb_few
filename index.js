05e675193a82e3a0ed9ecd1aa28df245ae525eea
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-31 08:45:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+let Scooter=function(year,color,model)
+{
+  this.year=year;
+  this.color=color;
+  this.model=model;
+}
+
+let Driver=function(name,age,experience)
+{
+  this.name=name;
+  this.age=age;
+  this.experience=experience;
+  }
+  
+  let PickupLocation=function(address, city)
+  {
+    this.address=address;
+    this.city=city;
+  }
